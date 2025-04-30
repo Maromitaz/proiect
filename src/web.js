d9813e2635cd61eb1a14eb85c9a6641a2bb45838
Maromitaz <boltasualex123@gmail.com>
2025-04-30 22:45:25 +0300

    buna

--- src/web.js
+++ src/web.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const fs = require("fs");
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    fs.readFile("./src/index.html", "utf-8", (err, data) => {
+        if(err){
+            console.error(err);
+        }
+        res.writeHead(200, {'content-type':'text/html'});
+        res.write(data);
+        return res.end();
+    });
+
+    // res.send('Hello World!');
+
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
